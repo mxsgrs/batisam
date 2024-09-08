a44a27d24bb74cdc4aea49061991c36657960e7f
Maxime Gros <mxs.grs@gmail.com>
2024-09-09 00:00:00 +0200

    HTML language

--- batisam-nextjs/src/app/layout.tsx
+++ batisam-nextjs/src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body className={inter.className}>
         <div className="flex flex-col min-h-screen">
           <Navbar />
